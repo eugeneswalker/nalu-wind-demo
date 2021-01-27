b61c28b3af2bd76530cabad84a40ad5225533278
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-01-27 14:54:59 -0800

    trust gpg key

--- uoregon/spectrum-mpi/setup.sh
+++ uoregon/spectrum-mpi/setup.sh
@@ -8,11 +8,11 @@ git clone https://github.com/spack/spack
 . spack/share/spack/setup-env.sh
 
 cp compiler-spack.yaml spack.yaml
+spack -e . buildcache keys -it
 spack -e . install --cache-only
 spack compiler add $(spack location -i gcc@7.4.0)
 
 cp -r ./netcdf-c spack/var/spack/repos/builtin/packages/
 cp nalu-wind-spack.yaml spack.yml
-spack -e . buildcache keys -it
 spack -e . concretize -f | tee concretize.log
 time spack -e . install
