ce6125327bdcd56b388df188adc9470170506f12
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-01-27 17:27:06 -0500

    add netcdf-c package patch for spectrum-mpi

--- summit/setup.sh
+++ summit/setup.sh
@@ -2,12 +2,16 @@
 
 SPACK_REF=5af2b1a9338563b4d382a9e12ce70537075c8363
 
-#git clone https://github.com/spack/spack
+git clone https://github.com/spack/spack
 (
  cd spack
  git checkout ${SPACK_REF}
  . share/spack/setup-env.sh
 )
+
+cp -r ./netcdf-c spack/var/spack/repos/builtin/packages/
+
+. spack/share/spack/setup-env.sh
 
 spack -e . buildcache keys -it
 
