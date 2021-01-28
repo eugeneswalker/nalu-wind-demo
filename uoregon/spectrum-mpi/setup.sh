93428d885ace996d67741ef4c167f43e86563923
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-01-27 17:07:41 -0800

    update uoregon/spectrum-mpi scripts

--- uoregon/spectrum-mpi/setup.sh
+++ uoregon/spectrum-mpi/setup.sh
@@ -7,12 +7,18 @@ git clone https://github.com/spack/spack
 
 . spack/share/spack/setup-env.sh
 
-cp compiler-spack.yaml spack.yaml
+cp gcc-spack.yaml spack.yaml
 spack -e . buildcache keys -it
 spack -e . install --cache-only
 spack compiler add $(spack location -i gcc@7.4.0)
 
+rm -rf spack.lock spack.yaml .spack-env
 cp -r ./netcdf-c spack/var/spack/repos/builtin/packages/
-cp nalu-wind-spack.yaml spack.yml
-spack -e . concretize -f | tee concretize.log
-time spack -e . install
+cp nalu-wind-spack.yaml spack.yaml
+spack -e . concretize -f | tee nalu-wind.dag
+time spack -e . install --cache-only
+
+git clone https://github.com/exawind/nalu-wind
+(cd nalu-wind && git submodule update --init --recursive)
+
+
