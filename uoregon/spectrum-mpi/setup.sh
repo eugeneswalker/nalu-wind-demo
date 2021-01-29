ea556b89c7c1b89c0b86639b1ea620704213b158
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-01-29 11:28:04 -0800

    update nalu-wind^spectrummpi recipe for cyclops [working]

--- uoregon/spectrum-mpi/setup.sh
+++ uoregon/spectrum-mpi/setup.sh
@@ -1,27 +1,35 @@
 #!/bin/bash -e
 
 SPACK_REF=5af2b1a9338563b4d382a9e12ce70537075c8363
+NALU_REF=414e27424ba6861c84a8a894b14062a83ac6ec56
 
 git clone https://github.com/spack/spack
-(cd spack && git checkout ${SPACK_REF})
+(
+ cd spack
+ git checkout ${SPACK_REF}
+)
 
 . spack/share/spack/setup-env.sh
 
-cp gcc-spack.yaml spack.yaml
-spack -e . buildcache keys -it
-spack -e . install --cache-only
+spack mirror add e4s-local /e4s-cache || :
+spack buildcache keys -it || :
+spack install --cache-only gcc@7.4.0 %gcc@4.8.5 target=ppc64le
 spack compiler add $(spack location -i gcc@7.4.0)
 
-rm -rf spack.lock spack.yaml .spack-env
 cp -r ./netcdf-c spack/var/spack/repos/builtin/packages/
-cp nalu-wind-spack.yaml spack.yaml
-spack -e . concretize -f | tee nalu-wind.dag
+spack -e . concretize -f | tee nw.dag
 time spack -e . install --cache-only
 
 git clone https://github.com/exawind/nalu-wind
-(cd nalu-wind && git submodule update --init --recursive)
+(
+ cd nalu-wind
+ git checkout ${NALU_REF}
+ git submodule update --init --recursive
+)
 
-if [ "${1}" == "1" ] ; then
-  spack load nalu-wind
-  ./run-all.sh
-fi
+
+
+#if [ "${1}" == "1" ] ; then
+#  spack load nalu-wind
+#  ./run-all.sh
+#fi
